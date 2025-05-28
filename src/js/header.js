4808d4c4db366f6622fc26f619211152de0f3081
Kyrylko <kkuzmenko1992@gmail.com>
2025-05-27 23:43:59 -0230

    pre production

--- src/js/header.js
+++ src/js/header.js
@@ -2,6 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const openBackdrop = document.querySelector('.burger-open-btn');
   const closeBackdrop = document.querySelector('.burger-close-btn');
   const backdrop = document.querySelector('.backdrop');
+  const navLinks = document.querySelectorAll('.bg-nav-list-link');
+
+  const path = window.location.pathname;
+
+  const isLightPage =
+    path.includes('cookies-policy.html') ||
+    path.includes('privacy-policy.html') ||
+    path.includes('terms-and-conditions.html');
+
+  if (isLightPage) {
+    document.body.classList.add('light-theme');
+
+
+    const burgerIcon = document.querySelector('.burger-menu-img');
+    if (burgerIcon) {
+      burgerIcon.src = './img/mobile/dark-burger-ico.png';
+    }
+  }
 
   function toggleMenu(show) {
     backdrop.classList.toggle('show', show);
@@ -16,4 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
   backdrop?.addEventListener('click', e => {
     if (e.target === backdrop) toggleMenu(false);
   });
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => toggleMenu(false));
+  });
+
+
+
 });
